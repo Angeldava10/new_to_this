81bc489be8e496c46c7f63c8be71f88e99e5481b
Angel David Santiago Nieves <angeld2341@gmail.com>
2022-10-20 21:15:03 -0400

    Add files via upload

---
+++ buffet.py
@@ -0,0 +1,11 @@
+#learn that the coma is outside of the list changes everything
+foods = ('fries' ,  'burgers' ,  'nuggets', 'pasta' , 'steak')
+print("You can choose from the following menu items:")
+for item in foods:
+    print("- " + item)
+
+
+foods = ('fries' ,  'burgers' ,  'nuggets', 'pasta' , 'steak' , 'pizza', 'rice')
+print("\nYou can choose fromtwo new items in our menu:")
+for item in foods:
+    print("- " + item)
