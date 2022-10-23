dc776711b3de7359354e337af63875eb6b4b0ca3
Angeldava10 <angeld2341@gmail.com>
2022-10-22 21:11:59 -0400

    Create alien_colors1.py

---
+++ alien_colors1.py
@@ -0,0 +1,16 @@
+#Practicing if-elif statements from what i learned in the book.
+
+alien_color = ['green', 'yellow', 'red']
+
+player_killed = 'green' #this mean the player killed a green alien
+player_killed= 'yellow' #this mean the player killed a yellow alien
+
+if player_killed in alien_color :
+    print('Great player! You have earned 5 points!')
+if player_killed in alien_color:
+    print('\nNice player, you have earned 3 points.')
+if alien_color == 'blue':
+    print('\nA new species have been found!')
+
+alien_color.append('blue')
+
