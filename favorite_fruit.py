2a754d0f611817ed593badbb26b459130666be58
Angeldava10 <angeld2341@gmail.com>
2022-10-23 13:14:36 -0400

    Create favorite_fruit.py

---
+++ favorite_fruit.py
@@ -0,0 +1,15 @@
+#writing a chain of if statments.
+
+favorite_fruits = ['grapes', 'apples', 'banana']
+
+#this if statements check in this fruit are on the list.
+if 'grapes' in favorite_fruits:
+    print('You really like grapes!')
+if 'organges' in favorite_fruits:
+    print('\nYou really like oranges!')
+if 'apples' in favorite_fruits:
+    print('\nYou really like apples!')
+if 'strawberry' in favorite_fruits:
+    print('\nYou really like strawberries!')
+if 'banana' in favorite_fruits:
+    print('\nYou really like bananas!')
