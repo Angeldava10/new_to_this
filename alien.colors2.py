2be69f9f6a24ca4209fbbd973ad2fadacd7d163f
Angeldava10 <angeld2341@gmail.com>
2022-10-22 21:24:36 -0400

    Create alien.colors2.py

---
+++ alien.colors2.py
@@ -0,0 +1,17 @@
+#pracricing if-else chain i learned in the book
+#this explains that a program skips things if they are not true 
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('Great player! You have earned 5 points.')
+else:
+    print('Congratulation player! You have earned 10 points!')
+
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('\nGreat player! You have earned 5 points.')
+else:
+    print('\nCongratulation player! You have earned 10 points!')
