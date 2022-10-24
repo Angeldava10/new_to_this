5744fdffea222a07128cc4575fbe97189349d13b
Angel David Santiago Nieves <angeld2341@gmail.com>
2022-10-23 21:05:18 -0400

    Delete even_numbers.py

--- even_numbers.py
+++
@@ -1,6 +0,0 @@
-#skip numbers in range() 
-even_numbers = list(range(2,11,2))
-print(even_numbers)
-#starts at 2 ends in 11, the final 2 is the number you sum the first 2 
-uneven_numbers = list(range(1,10,2))
-print(uneven_numbers)
