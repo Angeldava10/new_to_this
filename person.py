93966ab4ecb0de4bcfd03e5317825c9e5fa98db3
Angeldava10 <angeld2341@gmail.com>
2022-10-24 15:38:09 -0400

    Create person.py

---
+++ person.py
@@ -0,0 +1,12 @@
+#understanding how to use dictionary by myself. Trying to write clean code wtih the spaces 
+
+personal_info = {
+    'first_name': 'Angel',
+    'Last_name': '\nSantiago',
+    'age': '\n' + str(18),
+    'city': '\nGuayama',
+    }
+print(personal_info['first_name'] + 
+    personal_info['Last_name'] + 
+    personal_info['age'] +
+    personal_info['city'] )
