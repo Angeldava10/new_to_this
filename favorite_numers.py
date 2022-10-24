bd8f697e3267f343f084b60d71779ea7bb1e4851
Angeldava10 <angeld2341@gmail.com>
2022-10-24 15:38:00 -0400

    Create favorite_numers.py

---
+++ favorite_numers.py
@@ -0,0 +1,35 @@
+# Doing the same as person.py but with messages.
+# Need str() to be able to print in a sentence
+favorite_numbers = {
+    'Angel': str(5),
+    'Camille': str(4),
+    'Yuliana' : str(8),
+    'Anthony' : str(1),
+    'William': str(7),
+    } 
+
+print(
+    'Angel favorite number is ' + 
+    favorite_numbers['Angel'] +
+    '.' )
+
+print(
+    '\nCamille favorite number is' +
+    favorite_numbers['Camille'] +
+    '.')
+
+print(
+    '\nYuliana favrite number is ' + 
+    favorite_numbers['Yuliana'] + 
+    '.')
+
+print(
+    '\nWilliam favorite number is ' +
+    favorite_numbers['William'] + 
+    '.')
+
+print(
+    '\nAnthony favorite number is ' + 
+    favorite_numbers['Anthony'] + 
+    '.')
+
